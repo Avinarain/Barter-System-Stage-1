dd4363557fd2b012ea9e18ffd7584b55de2519a4
Avinarain <69110511+Avinarain@users.noreply.github.com>
2021-05-01 20:24:34 +0530

    Add files via upload

---
+++ barter_app_1/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
+
+export default class App extends React.Component{
+
+  render(){
+      
+      
+      return(
+        <WelcomeScreen/>
+      )
+  }
+
+ 
+
+}
